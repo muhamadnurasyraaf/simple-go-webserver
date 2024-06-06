603f1dd0121aef5d0ab524a686eec982bb7e637c
Asyraaf <masyraaf14@gmail.com>
2024-06-06 23:46:56 +0800

    handle post request

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	fmt.Fprintf(w, "Hello There!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	}
+	fmt.Fprintf(w, "POST Request Successful\n")
+
+	name := r.FormValue("name")
+
+	fmt.Fprintf(w, "Name : %s\n", name)
+}
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server at port 8070\n")
+	fmt.Printf("http://localhost:8070")
+
+	if err := http.ListenAndServe("127.0.0.1:8070", nil); err != nil {
+		log.Fatal(err)
+	}
+}
